f976f68ee727cf6d76d66c056f3da5143979ec75
Matthew L Daniel <md@stoi.cc>
2024-09-15 13:49:37 -0700

    Allow OpenMP to be opt-in

--- src/main.c
+++ src/main.c
@@ -1,7 +1,7 @@
 #include "string.h"
 #include "ui.h"
 
-#ifndef WEB
+#if defined(WITH_OPENMP)
 #include <omp.h>
 #endif
 
@@ -16,12 +16,14 @@ static Ui _ui = {0};
 int main()
 {
   UiLoad(&_ui);
+#if defined(WITH_OPENMP)
   // OpenMP is getting all CPU at Windows for some reason...
   // Add limit of 6 threads to reduce its effect until a better solution is
   // found.
   int nt = omp_get_max_threads();
   nt = nt < 6 ? nt : 6;
   omp_set_num_threads(nt);
+#endif
 
 #if defined(PLATFORM_WEB)
   emscripten_set_main_loop(web_update_draw_frame, 0, 1);
