f8bccc9ae79e75dd7d3a5a2b51e11e933324dec8
lets_all_be_stupid_forever <circuitartistgame@gmail.com>
2026-01-23 16:10:51 -0300

    fix shader wire2_frag.glsl not compiling

--- assets/shaders/wire2_frag.glsl
+++ assets/shaders/wire2_frag.glsl
@@ -26,10 +26,9 @@ float unpack_float32(vec4 rgba) {
 // Do I need to pass p0,p1,p? Or can i just pass p ?
 float interp(int p0, int p1, int p, float rc, float d0,
                            float d1) {
-
   if (d0 == 0 && d1 == 0) {
-return 0;
-}
+    return 0.f;
+  }
   float a, t0, t1;
   if (p0 == p) return d0;
   if (p1 == p) return d1;
